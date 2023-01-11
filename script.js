3b6e925598b8fb5ce9e7c95dca3afe12eff6f64f
Greg Trahan <trahan.greg@gmail.com>
2023-01-11 11:34:37 -0600

    update HTML tag loop

--- script.js
+++ script.js
@@ -1,32 +1,12 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the textArea in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 
+$(function () {
 
   start()
+
   function start(){
     currentTime = tellTime()
     currentHour = displayTime(currentTime)
-    toDoList = retreiveWork()
+    toDoList = retreiveWork(currentHour)
     saveWork(toDoList)
   }
 
@@ -42,12 +22,11 @@ $(function () {
     return currentHour
   }
 
-  function retreiveWork(){
+  function retreiveWork(currentHour){
     index = 0
-    let toDoList = []
-    var firstToDo = JSON.parse(localStorage.getItem("toDoList"));
-    if (firstToDo === null){
-      firstToDo = []
+    // let toDoList = []
+    let toDoList = JSON.parse(localStorage.getItem("toDoList"));
+    if (toDoList === null){
       toDoList=[
         {'hour': 'hour-8', 'text': ''},
         {'hour': 'hour-9', 'text': ''},
@@ -60,31 +39,40 @@ $(function () {
         {'hour': 'hour-16', 'text': ''},
         {'hour': 'hour-17', 'text': ''},
       ]
-      
-    } else {
-      toDoList = firstToDo
     }
 
     toDoList.forEach(function(item, index){
       //Dynamically build HTML tags
+      //Create current hour div tag
       $('.container-lg').append('<div id="hour-8" class="row time-block"></div>')
-      
+      if (index+8 < currentHour){
+        $(`#${item.hour}`).addClass(`past`)
+      } else if (index+8 === currentHour) {
+        $(`#${item.hour}`).addClass(`present`)
+      } else {
+        $(`#${item.hour}`).addClass(`future`)
+      }
+
+      //Create display time div tag
       $(`#${item.hour}`).append('<div class="col-2 col-md-1 hour text-center py-3"></div>')
-      if (index < 4){
+      if (currentHour < 12){
         $(`#${item.hour}`).children('.hour').append(`${index+8}AM`)
       } else {
         $(`#${item.hour}`).children('.hour').append(`${index+8}PM`)
       }
       
+      //Create text area tag
       $(`#${item.hour}`).append('<textarea class="col-8 col-md-10 textArea" rows="3"></textarea>')
       //Add Text
+      $(`#${item.hour}`).children('.textArea').append(toDoList[index].text)
         // firstToDo[index].text //This is the text I want to add
         // $(`#${item.hour}`).children('.textArea') // This targets the text area
         // $('.john').append('Philly') //This is  how I add the text
-      $(`#${item.hour}`).children('.textArea').append(firstToDo[index].text)
 
+      //Create button tag
       $(`#${item.hour}`).append('<button class="btn saveBtn col-2 col-md-1" aria-label="save">')
 
+      //create i tag
       $(`#${item.hour}`).children('.saveBtn').append('<i class="fas fa-save" aria-hidden="true"></i>')
 
     })
@@ -107,14 +95,3 @@ $(function () {
 
 
 });
-
-
-/* 
-<div id="hour-11" class="row time-block future">
-  <div class="col-2 col-md-1 hour text-center py-3">11AM</div>
-  <textarea class="col-8 col-md-10 textArea" rows="3"> </textarea>
-  <button class="btn saveBtn col-2 col-md-1" aria-label="save">
-    <i class="fas fa-save" aria-hidden="true"></i>
-  </button>
-</div> 
-*/
